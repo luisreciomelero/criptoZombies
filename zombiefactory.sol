c24c51c8e618e731a26dffcecb9646d27876035f
Luis Recio Melero <luisreciomelero@MacBook-Pro-de-Luis.local>
2018-10-11 12:33:47 +0200

    criptoZombies2_comentado

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.19;
 
 contract ZombieFactory {
 
-    event NewZombie(uint zombieId, string name, uint dna);
+    event NewZombie(uint zombieId, string name, uint dna); //Evento que notifica la creacion de un nuevo Zombie
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
@@ -14,26 +14,26 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    mapping (uint => address) public zombieToOwner;
-    mapping (address => uint) ownerZombieCount;
+    mapping (uint => address) public zombieToOwner; //diccionarios que iremos guardando (de forma clave-valor) a quien pertenece un zombie. clave-Zombie1 => valor-direccion del propietario.
+    mapping (address => uint) ownerZombieCount; //diccionario de cuantos zombies tiene un usuario. Clave-direccion_usuario => valor- Numero de zombies
 
-    function _createZombie(string _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
-        zombieToOwner[id] = msg.sender;
-        ownerZombieCount[msg.sender]++;
-        NewZombie(id, _name, _dna);
+    function _createZombie(string _name, uint _dna) internal { //hemos cambiado el metodo a internal para que al heredar otro contrado de este pueda usarlo
+        uint id = zombies.push(Zombie(_name, _dna)) - 1; //Añadimos el zombie al array y nos quedamos con la posicion de este
+        zombieToOwner[id] = msg.sender; //Asignamos el zombie creado a la direccion del que lo ha creado.
+        ownerZombieCount[msg.sender]++; //aumentamos en 1 el registro de zombies que tenia el creador
+        NewZombie(id, _name, _dna); //Una vez creado llamamos al evento
     }
 
-    function _generateRandomDna(string _str) private view returns (uint) {
+    function _generateRandomDna(string _str) private view returns (uint) { //funcion que genera a partir del nombre un ADN. Lo genera a partir de cualquier string pero será del nombre
         uint rand = uint(keccak256(_str));
         return rand % dnaModulus;
     }
 
     function createRandomZombie(string _name) public {
-        require(ownerZombieCount[msg.sender] == 0);
-        uint randDna = _generateRandomDna(_name);
-        randDna = randDna - randDna % 100;
-        _createZombie(_name, randDna);
+        require(ownerZombieCount[msg.sender] == 0); //comprobamos que el creador no tiene mas zombies creados de esta forma. Require lanza error si no se cumple la condicion.
+        uint randDna = _generateRandomDna(_name); 
+        randDna = randDna - randDna % 100; // modificamos el ADN generado dejando a 0 el ADN de los zombies en las 2 ultimas cifras. Mas adelante modificaremos el de los zombies gato.
+        _createZombie(_name, randDna); //finalmente creamos el zombie.
     }
 
 }
--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.4.19;
 
-import "./zombiefactory.sol";
+import "./zombiefactory.sol"; //este contrato esta heredado, por ello tenemos que importar el contrato padre.
 
-contract KittyInterface {
+contract KittyInterface { // interfaz contrato que traemos de las criptokittys
   function getKitty(uint256 _id) external view returns (
     bool isGestating,
     bool isReady,
@@ -17,24 +17,24 @@ contract KittyInterface {
   );
 }
 
-contract ZombieFeeding is ZombieFactory {
+contract ZombieFeeding is ZombieFactory { //declaración del contrato hijo
 
-  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-  KittyInterface kittyContract = KittyInterface(ckAddress);
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d; //dirección del contrato de CryptoKitties, será el encargado de enlazar la interfaz Kitty con nuestro código
+  KittyInterface kittyContract = KittyInterface(ckAddress); //una vez tenemos la direccion enlazamos.
 
-  // Modifica la definición de la función aquí:
+  
   function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
-    _targetDna = _targetDna % dnaModulus;
+    _targetDna = _targetDna % dnaModulus; //nos quedamos con las últimos 16 dígitos del ADN pasado
     uint newDna = (myZombie.dna + _targetDna) / 2;
-    if(keccak256(_species) == keccak256("kitty")){
+    if(keccak256(_species) == keccak256("kitty")){ //como lo comentamos en la clase Factory, modificamos las 2 últimas cifras del ADN del kittyZombie
       newDna = newDna - newDna % 100 + 99;
     }
     _createZombie("NoName", newDna);
   }
 
-  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+  function feedOnKitty(uint _zombieId, uint _kittyId) public { //Este método nos permite acceder al ADN del kitty guardado en el contrato de las kittys.
     uint kittyDna;
     (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
     
